620041194e0e80723069ed6f6ac19951f51f240e
penprapa-m <45387416+penprapa-m@users.noreply.github.com>
2020-06-02 09:22:25 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+ const add = (x,y) => x + y;
+ const multiply = (x,y) => (x*y);
+
+ const getName = () => {
+    return 'Jim';
+  };
+
+exports.add = add;
+exports.multiply = multiply;
+exports.getName = getName;
